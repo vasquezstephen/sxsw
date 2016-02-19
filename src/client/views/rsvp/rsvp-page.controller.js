db993889f6147535698d3af9cf18cd385331c0d0
Stephen V <stephenrvasquez@gmail.com>
2016-02-19 02:51:49 -0600

    Added tab flip functionality Added expand tab functionality Still need collapse tab

--- src/client/views/rsvp/rsvp-page.controller.js
+++ src/client/views/rsvp/rsvp-page.controller.js
@@ -9,10 +9,25 @@
         .config(configRoutes)
         .controller('RSVPController', RSVPController);
 
-    RSVPController.$inject = ['$log','$scope','$location','$anchorScroll','$timeout','DBServices'];
-    function RSVPController($log, $scope, $location, $anchorScroll, $timeout ,DBServices) {
+    RSVPController.$inject = ['$log', '$scope', '$location', '$anchorScroll', '$timeout', 'DBServices'];
+    function RSVPController($log, $scope, $location, $anchorScroll, $timeout, DBServices) {
         var rsvp = this;
-        rsvp.openDivider = false;
+        rsvp.tab = 1;
+        rsvp.prevTab = null;
+        rsvp.limit = {
+            i1: 8,
+            i10: 4,
+            i11: 4,
+            i12: 4,
+            i13: 4,
+            i14: 4,
+            i15: 4,
+            i16: 4,
+            i17: 4,
+            i18: 4,
+            i19: 4,
+            i20: 4
+        };
         var populateList = function (data) {
             rsvp.list = data;
         };
@@ -21,20 +36,33 @@
             .then(populateList);
         rsvp.scrollTo = scrollTo;
         rsvp.returnToTop = returnToTop;
+        rsvp.setTab = setTab;
+        rsvp.isSet = isSet;
 
 
         function scrollTo(id) {
-            $timeout(function(){
+            $timeout(function () {
                 $location.hash(id);
                 $anchorScroll();
             });
 
         }
+
         function returnToTop() {
             $anchorScroll.yOffset = 0;
             $anchorScroll();
         }
+
+        function setTab(tabId) {
+            rsvp.prevTab = rsvp.tab;
+            rsvp.tab = tabId;
+        }
+
+        function isSet(tabId) {
+            return rsvp.tab === tabId;
+        }
     }
+
     configRoutes.$inject = ['$stateProvider'];
     function configRoutes($stateProvider) {
         $stateProvider
